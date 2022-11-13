d617b8b032f4fd03591ce4d7a3039522ff80472b
mohammed berbar <mohamedbarbarr@gmail.com>
2022-11-13 18:33:56 +0100

    first commit

---
+++ src/main/java/com/example/sayhello/controller/sayHelloController.java
@@ -0,0 +1,12 @@
+package com.example.sayhello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class sayHelloController {
+    @GetMapping("sayhello")
+    public String sayHello(){
+        return "say hello";
+    }
+}
