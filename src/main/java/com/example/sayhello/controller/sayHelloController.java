78222c8be0fabab40e9b8c9055e01a688cac509a
mohammed berbar <mohamedbarbarr@gmail.com>
2023-01-03 20:09:47 +0100

    58

--- src/main/java/com/example/sayhello/controller/sayHelloController.java
+++ src/main/java/com/example/sayhello/controller/sayHelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class sayHelloController {
     @GetMapping("sayhello")
     public String sayHello(){
-        return "say hello 57";
+        return "say hello 58";
     }
 }
