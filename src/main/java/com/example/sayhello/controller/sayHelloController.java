ea2efb058924fd42cf4b1692d43999799af75e99
The-Shinigami <mohamedbarbarr@gmail.com>
2023-01-04 17:39:56 +0100

    75

--- src/main/java/com/example/sayhello/controller/sayHelloController.java
+++ src/main/java/com/example/sayhello/controller/sayHelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class sayHelloController {
     @GetMapping("sayhello")
     public String sayHello(){
-        return "say hello 74";
+        return "say hello 75";
     }
 }
