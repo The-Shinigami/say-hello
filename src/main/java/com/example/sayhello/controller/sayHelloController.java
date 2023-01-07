d80b46313c6f6c350b97c021d90d270cd75bb7cd
The-Shinigami <mohamedbarbarr@gmail.com>
2023-01-07 07:47:01 +0100

    77

--- src/main/java/com/example/sayhello/controller/sayHelloController.java
+++ src/main/java/com/example/sayhello/controller/sayHelloController.java
@@ -2,11 +2,12 @@ package com.example.sayhello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 public class sayHelloController {
-    @GetMapping("sayhello")
-    public String sayHello(){
-        return "say hello 76";
+    @GetMapping("sayhello/{name}")
+    public String sayHello(@PathVariable String name){
+        return "say hello "+name;
     }
 }
