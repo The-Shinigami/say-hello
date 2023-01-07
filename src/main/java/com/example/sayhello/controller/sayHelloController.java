17973041e76d498d5bc1bd1a22f6a95215feefbf
The-Shinigami <mohamedbarbarr@gmail.com>
2023-01-07 09:18:16 +0100

    78

--- src/main/java/com/example/sayhello/controller/sayHelloController.java
+++ src/main/java/com/example/sayhello/controller/sayHelloController.java
@@ -3,8 +3,10 @@ package com.example.sayhello.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "+")
 public class sayHelloController {
     @GetMapping("sayhello/{name}")
     public String sayHello(@PathVariable String name){
